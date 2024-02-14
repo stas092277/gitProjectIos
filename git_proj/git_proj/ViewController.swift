979a5676984f7f9687311174cd12e9fcfb6c7aea
Stacg Chebykin <02stas02@gmail.com>
2024-02-15 01:57:31 +0300

    add all

---
+++ git_proj/git_proj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git_proj
+//
+//  Created by Стас Чебыкин on 15.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
